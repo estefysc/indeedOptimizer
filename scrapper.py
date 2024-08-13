75326e1ce289b360c6a32174fcdb367eea8f4577
Estefania <estefysc@gmail.com>
2024-08-13 10:52:37 -0400

    removed unnecessary imports

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,6 @@ import os
 import re
 import time
 
-from typing import List
 from urllib.parse import urlencode
 from scrapfly import ScrapflyClient, ScrapeConfig
 from ordered_set import OrderedSet
