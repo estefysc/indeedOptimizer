0bcdf680f7057363b5db152e292c8bc3a1582f43
Estefania <estefysc@gmail.com>
2024-09-27 13:47:06 -0400

    eliminated details

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from scheduler import start_scheduler
+from scheduler import start_scheduler, run_one_time_scrape
 
 if __name__ == "__main__":
-    start_scheduler()
+    # errors = red
+    # gui = blue
+    # scheduler = green
+    # redis = yellow    
+    # scrape = magenta
+    tasks = [
+        ("software_developer", "sarasota"),
+        ("software_engineer", "sarasota"),
+        ("software_developer", "tampa"),
+        ("software_engineer", "tampa")
+    ]
+    run_every_minutes = 3
+    staggering_minutes = 5
+
+    start_scheduler(tasks, run_every_minutes, staggering_minutes)
+    # run_one_time_scrape("software_development", "tampa")
